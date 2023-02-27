0fc34599bd01b90f2c8e5a7bc3286be600b9f7b9
jannorth <116915245+jannorth@users.noreply.github.com>
2023-02-27 13:30:13 -0500

    setting up models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,34 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
+const Comment = require("./Comment")
 
 User.hasMany(BlogPost, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CAASCADE"
+})
+
+BlogPost.hasMany(Comment, {
+  foreignKey: "blogPost_id",
+  onDelete: "CASCADE"
+})
+
 BlogPost.belongsTo(User, {
   foreignKey: 'user_id'
 });
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: "blogPost_id",
+  onDelete: "CASCADE"
+})
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE"
+})
 
 module.exports = { User, BlogPost };
